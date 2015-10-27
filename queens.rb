2b128b4650b8729fbf6db537682285de423b1908
Dwight Ware <dwightware4@gmail.com>
2015-10-27 15:55:42 -0700

    cleaned up code

--- queens.rb
+++ queens.rb
@@ -4,20 +4,16 @@ class Board
   QUEENS = 2
 
   def initialize
-    @queens = []
-    @valid_moves = generate_valid_moves_array
+    @queens, @valid_moves = [], generate_valid_moves_array
   end
 
   def select_move
     result = nil
+
     @valid_moves.each do |move|
       temp_queens = @queens.dup
       temp_queens << move
     end
-  end
-
-  def place_queen(row, col)
-    
   end
 
   def remove_invalid_moves
@@ -26,11 +22,14 @@ class Board
 
   def generate_valid_moves_array
     valid_moves = []
+
     (0..BOARD_LENGTH).to_a.each do |x|
       (0..BOARD_LENGTH).to_a.each do |y|
         valid_moves << [x,y]
       end
     end
+
+    valid_moves
   end
 
   def valid_placement?(row, col)
@@ -44,10 +43,7 @@ class Board
     @queens.any? do |queen|
       queen_x = queen[0]
       queen_y = queen[1]
-      (x - y) == (queen_x - queen_y) ||
-      (x - y) == (queen_y - queen_x)
+      (x - y) == (queen_x - queen_y) || (x - y) == (queen_y - queen_x)
     end
   end
-
-
 end
